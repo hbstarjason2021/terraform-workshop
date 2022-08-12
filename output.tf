452b41b5d285fb1846e3238b19efdfcc59b070fe
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-08-13 01:38:10 +0800

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,10 @@
+output "deploy_time" {
+    description = "the deployement time, in utc"
+    value = timestamp()
+}
+
+### terraform validate
+
+### terraform console
+### type(timestamp())
+### terraform output deploy_time
