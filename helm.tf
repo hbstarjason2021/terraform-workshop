2ea3405fdef0505b9a90881baeb40193ab7801c1
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-08-13 00:56:41 +0800

    Create helm.tf

---
+++ helm.tf
@@ -0,0 +1,22 @@
+### https://registry.terraform.io/providers/hashicorp/helm/latest/docs
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "nginx_ingress" {
+  name       = "nginx-ingress-controller"
+
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "nginx-ingress-controller"
+
+  set {
+    name  = "service.type"
+    value = "ClusterIP"
+  }
+}
+
+
+
