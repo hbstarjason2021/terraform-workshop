36742b0d7fd2597a3ba2df75064a0f4b5f190476
hbstarjason2021 <1124484297@qq.com>
2023-04-13 14:58:16 +0000

    add huaweicloud vpc

---
+++ huaweicloud/vpc/main.tf
@@ -0,0 +1,25 @@
+
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    huaweicloud = {
+      ### 
+      source  = "local-registry/huaweicloud/huaweicloud"
+      version = ">= 1.20.0"
+    }
+  }
+}
+
+# Configure the HuaweiCloud Provider
+provider "huaweicloud" {
+  region     = "cn-north-4"
+  access_key = "XXX"
+  secret_key = "XXX"
+}
+
+# Create a VPC
+resource "huaweicloud_vpc" "vpcdemo" {
+  name = "terraform_vpc"
+  cidr = "192.168.0.0/16"
+}
