385591fe71396e689d7e39c2e471f5c55b518436
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-04-13 23:10:29 +0800

    Update main.tf

--- huaweicloud/vpc/main.tf
+++ huaweicloud/vpc/main.tf
@@ -1,3 +1,8 @@
+### https://support.huaweicloud.com/qs-terraform/index.html
+
+## export HW_REGION_NAME="cn-north-4"
+## export HW_ACCESS_KEY="my-access-key"
+## export HW_SECRET_KEY="my-secret-key"
 
 terraform {
   required_version = ">= 0.13"
@@ -14,8 +19,8 @@ terraform {
 # Configure the HuaweiCloud Provider
 provider "huaweicloud" {
   region     = "cn-north-4"
-  access_key = "XXX"
-  secret_key = "XXX"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
 }
 
 # Create a VPC
