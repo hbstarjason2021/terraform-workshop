61787154f8633b0d4e403aa750ebe2182d97703d
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-04-15 19:47:16 +0800

    Update provider.tf

--- huaweicloud/cce/provider.tf
+++ huaweicloud/cce/provider.tf
@@ -18,3 +18,8 @@ provider "huaweicloud" {
   secret_key = "XXX"
 }
 
+
+provider "kubernetes" {
+            config_path    = local_file.kube_config.filename
+            config_context = "external"
+}
