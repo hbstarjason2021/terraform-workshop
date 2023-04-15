79b133a63d9fde7d52e455167a91c02332c1f48b
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-04-16 01:12:15 +0800

    Update variables.tf

--- huaweicloud/ecs/variables.tf
+++ huaweicloud/ecs/variables.tf
@@ -1,12 +1,12 @@
-variable "region" {
-  type    = string
-  default = "cn-north-4"
-}
-
 
 variable "hw_access_key" {
     type = "string"
 }
 variable "hw_secret_key" {
     type = "string"
+}
+
+variable "hw_region" {
+  type    = string
+  default = "cn-north-4"
 }
