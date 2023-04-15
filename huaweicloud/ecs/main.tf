edecdd1d855dea8dad8e1663f12938b446d2ddfb
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-04-16 02:40:37 +0800

    Update main.tf

--- huaweicloud/ecs/main.tf
+++ huaweicloud/ecs/main.tf
@@ -16,13 +16,26 @@ data "huaweicloud_images_image" "myimage" {
   most_recent = true
 }
 
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "!@"
+  min_numeric      = 1
+  min_lower        = 1
+  min_special      = 1
+}
+
 resource "huaweicloud_compute_instance" "myinstance" {
-  name              = "basic"
-  image_id          = data.huaweicloud_images_image.myimage.id
-  flavor_id         = data.huaweicloud_compute_flavors.myflavor.ids[0]
+  name               = "basic"
+  image_id           = data.huaweicloud_images_image.myimage.id
+  flavor_id          = data.huaweicloud_compute_flavors.myflavor.ids[0]
   #security_groups   = ["default"]
-  availability_zone = data.huaweicloud_availability_zones.myaz.names[0]
-  #admin_pass        = "Huawei123"  
+  #security_group_ids = [huaweicloud_networking_secgroup.default.id]
+  
+  availability_zone  = data.huaweicloud_availability_zones.myaz.names[0]
+  system_disk_type   = "SSD"
+  #admin_pass        = "Huawei123" 
+  #admin_pass        = random_password.password.result
 
   network {
     uuid = data.huaweicloud_vpc_subnet.mynet.id
