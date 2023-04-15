80823c5a711ab44187c78d45cf5dbd0ad55e2780
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-04-15 22:56:33 +0800

    Update provider.tf

--- huaweicloud/ecs/provider.tf
+++ huaweicloud/ecs/provider.tf
@@ -8,8 +8,10 @@ terraform {
 
   required_providers {
     huaweicloud = {
-      ### source = "huaweicloud/huaweicloud"
-      source  = "local-registry/huaweicloud/huaweicloud"
+      source = "huaweicloud/huaweicloud"
+      
+      ##source  = "local-registry/huaweicloud/huaweicloud"
+      
       version = ">= 1.20.0"
     }
   }
