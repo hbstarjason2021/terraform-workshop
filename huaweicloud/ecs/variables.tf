a3a835d699e3b546e897731288831a22fe490479
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-04-27 10:42:57 +0800

    Update variables.tf

--- huaweicloud/ecs/variables.tf
+++ huaweicloud/ecs/variables.tf
@@ -10,3 +10,31 @@
 #  type    = string
 #  default = "cn-north-4"
 #}
+
+variable "vpc_name" {
+  default = "vpc-basic"
+}
+
+variable "vpc_cidr" {
+  default = "172.16.0.0/16"
+}
+
+variable "subnet_name" {
+  default = "subent-default"
+}
+
+variable "subnet_cidr" {
+  default = "172.16.10.0/24"
+}
+
+variable "subnet_gateway" {
+  default = "172.16.10.1"
+}
+
+variable "primary_dns" {
+  default = "100.125.1.250"
+}
+
+variable "secondary_dns" {
+  default = "100.125.21.250"
+}
