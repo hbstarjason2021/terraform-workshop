35234115104e998fc823e27f93f5f9746631a4b1
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-05-19 11:06:42 +0800

    Update main.tf

--- huaweicloud/cce/main.tf
+++ huaweicloud/cce/main.tf
@@ -28,10 +28,6 @@ resource "huaweicloud_vpc_eip" "myeip" {
 
 data "huaweicloud_availability_zones" "myaz" {}
 
-resource "huaweicloud_compute_keypair" "mykeypair" {
-  name = var.key_pair_name
-}
-
 
 ## https://support.huaweicloud.com/usermanual-terraform/terraform_0015.html
 ## https://registry.terraform.io/providers/huaweicloud/huaweicloud/latest/docs/data-sources/cce_cluster
@@ -48,55 +44,6 @@ resource "huaweicloud_cce_cluster" "mycce" {
   eip                    = huaweicloud_vpc_eip.myeip.address
 }
 
-resource "huaweicloud_cce_node" "mynode" {
-  cluster_id        = huaweicloud_cce_cluster.mycce.id
-  name              = var.node_name
-  flavor_id         = var.node_flavor
-  availability_zone = data.huaweicloud_availability_zones.myaz.names[0]
-  key_pair          = huaweicloud_compute_keypair.mykeypair.name
-
-  root_volume {
-    size       = var.root_volume_size
-    volumetype = var.root_volume_type
-  }
-  data_volumes {
-    size       = var.data_volume_size
-    volumetype = var.data_volume_type
-  }
-}
-
-data "huaweicloud_images_image" "myimage" {
-  name        = var.image_name
-  most_recent = true
-}
-
-resource "huaweicloud_compute_instance" "myecs" {
-  name                        = var.ecs_name
-  image_id                    = data.huaweicloud_images_image.myimage.id
-  flavor_id                   = var.ecs_flavor
-  availability_zone           = data.huaweicloud_availability_zones.myaz.names[0]
-  key_pair                    = huaweicloud_compute_keypair.mykeypair.name
-  delete_disks_on_termination = true
-
-  system_disk_type = var.root_volume_type
-  system_disk_size = var.root_volume_size
-
-  data_disks {
-    type = var.data_volume_type
-    size = var.data_volume_size
-  }
-
-  network {
-    uuid = huaweicloud_vpc_subnet.mysubnet.id
-  }
-}
-
-resource "huaweicloud_cce_node_attach" "test" {
-  cluster_id = huaweicloud_cce_cluster.mycce.id
-  server_id  = huaweicloud_compute_instance.myecs.id
-  key_pair   = huaweicloud_compute_keypair.mykeypair.name
-  os         = var.os
-}
 
 
 resource "local_file" "kube_config" {
