d88df2a7be098972da2196d4e4b71309b51861a1
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-05-19 11:14:26 +0800

    Update main.tf

--- huaweicloud/cce/main.tf
+++ huaweicloud/cce/main.tf
@@ -1,30 +1,4 @@
-resource "huaweicloud_vpc" "ccevpc" {
-  name = var.vpc_name
-  cidr = var.vpc_cidr
-}
 
-resource "huaweicloud_vpc_subnet" "mysubnet" {
-  name       = var.subnet_name
-  cidr       = var.subnet_cidr
-  gateway_ip = var.subnet_gateway
-
-  # dns is required for cce node installing
-  primary_dns   = var.primary_dns
-  secondary_dns = var.secondary_dns
-  vpc_id        = huaweicloud_vpc.ccevpc.id
-}
-
-resource "huaweicloud_vpc_eip" "myeip" {
-  publicip {
-    type = "5_bgp"
-  }
-  bandwidth {
-    name        = var.bandwidth_name
-    size        = 8
-    share_type  = "PER"
-    charge_mode = "traffic"
-  }
-}
 
 data "huaweicloud_availability_zones" "myaz" {}
 
