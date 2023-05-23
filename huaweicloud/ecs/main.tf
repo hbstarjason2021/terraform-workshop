574ab2aeb9d7ced28b0677ad835111bd9e8de48a
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-05-23 09:54:47 +0800

    Update main.tf

--- huaweicloud/ecs/main.tf
+++ huaweicloud/ecs/main.tf
@@ -1,3 +1,15 @@
+# 定义一些变量，时间戳的作用是避免命名重复。 使用时调用如： ${local.timestamp}
+locals {
+	timestamp = formatdate("YYYYMMDDhhmmss", timestamp())
+	# ecs 服务器的密码
+	# ecspassword="${random_string.password.result}@wm"
+	# obs的名称
+	obsname = "ccebackup-${local.timestamp}"
+  # obs桶策略
+  obspolicy = jsonencode({"Statement":[{"Sid": "公共读-${local.obsname}","Effect": "Allow","Principal": {"ID": ["*"]},"Action": ["ListBucket","HeadBucket","GetBucketLocation","ListBucketVersions","GetObject","RestoreObject","GetObjectVersion"],"Resource": ["${local.obsname}","${local.obsname}/*"]}]})
+}
+
+
 data "huaweicloud_availability_zones" "myaz" {}
 
 data "huaweicloud_compute_flavors" "myflavor" {
@@ -77,6 +89,7 @@ EOF
   }
 }
 
+
 resource "huaweicloud_vpc_eip" "myeip" {
   publicip {
     type = "5_bgp"
@@ -105,4 +118,17 @@ resource "huaweicloud_evs_volume" "myvolume" {
 resource "huaweicloud_compute_volume_attach" "attached" {
   instance_id = huaweicloud_compute_instance.myinstance.id
   volume_id   = huaweicloud_evs_volume.myvolume.id
+}
+
+
+## https://support.huaweicloud.com/usermanual-terraform/terraform_0012.html
+# 创建obs桶
+resource "huaweicloud_obs_bucket" "obs" {
+  bucket = local.obsname
+  acl    = "public-read"
+  tags = {
+    type = "bucket"
+    env  = "${local.obsname}"
+  }
+  policy = local.obspolicy
 }
