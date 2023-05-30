9176b21ab977f73eebff331eeaf643c77a0f4f71
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-05-30 14:35:43 +0800

    Update main.tf

--- huaweicloud/ecs/main.tf
+++ huaweicloud/ecs/main.tf
@@ -121,14 +121,3 @@ resource "huaweicloud_compute_volume_attach" "attached" {
 }
 
 
-## https://support.huaweicloud.com/usermanual-terraform/terraform_0012.html
-# 创建obs桶
-resource "huaweicloud_obs_bucket" "obs" {
-  bucket = local.obsname
-  acl    = "public-read"
-  tags = {
-    type = "bucket"
-    env  = "${local.obsname}"
-  }
-  policy = local.obspolicy
-}
