5da825c2b49705551a253b3f40798b94db373df1
hbstarjason2021 <1124484297@qq.com>
2023-06-25 16:49:22 +0000

    add cce-turbo files

---
+++ huaweicloud/cce-turbo/provider.tf
@@ -0,0 +1,25 @@
+
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    huaweicloud = {
+      source = "huaweicloud/huaweicloud"
+      ### source  = "local-registry/huaweicloud/huaweicloud"
+      version = ">= 1.20.0"
+    }
+  }
+}
+
+# Configure the HuaweiCloud Provider
+provider "huaweicloud" {
+  region     = "cn-north-4"
+  #access_key = "XXX"
+  #secret_key = "XXX"
+}
+
+
+#provider "kubernetes" {
+#            config_path    = local_file.kube_config.filename
+#            config_context = "external"
+#}
