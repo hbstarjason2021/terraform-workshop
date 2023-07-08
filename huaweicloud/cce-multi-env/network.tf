aaf5b3a375bdbbbc49e8d8ee562431405e31f85e
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-07-08 17:44:56 +0800

    Update network.tf

--- huaweicloud/cce-multi-env/network.tf
+++ huaweicloud/cce-multi-env/network.tf
@@ -1,1 +1,15 @@
+resource "huaweicloud_vpc" "ccevpc" {
+  name = var.ccevpc_name
+  cidr = var.ccevpc_cidr
+}
 
+resource "huaweicloud_vpc_subnet" "ccesubnet" {
+  name       = var.ccesubnet_name
+  cidr       = var.ccesubnet_cidr
+  gateway_ip = var.ccesubnet_gateway
+
+  # dns is required for cce node installing
+  primary_dns   = var.primary_dns
+  secondary_dns = var.secondary_dns
+  vpc_id        = huaweicloud_vpc.ccevpc.id
+}
