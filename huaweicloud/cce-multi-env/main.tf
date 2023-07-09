ad1f338bb01c7c86d6496f296e10723844a0e59c
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-07-09 19:44:39 +0800

    Create main.tf

---
+++ huaweicloud/cce-multi-env/main.tf
@@ -0,0 +1,10 @@
+### terraform init
+### terraform workspace new dev
+### terraform workspace show
+### terraform workspace list
+### terraform workspace select dev
+### terraform plan -var-file=terraform.dev.tfvars -out tfplan
+### terraform apply tfplan
+
+### terraform workspace select dev
+### terraform destroy -var-file=terraform.dev.tfvars 
