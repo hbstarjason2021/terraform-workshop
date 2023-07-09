1bc41014e8b690d6ac9f3d53c58313968cff742c
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-07-09 18:57:22 +0800

    Update network.tf

--- huaweicloud/cce-multi-env/network.tf
+++ huaweicloud/cce-multi-env/network.tf
@@ -1,10 +1,10 @@
 resource "huaweicloud_vpc" "ccevpc" {
-  name = "vpc-${var.env_name}"
+  name = "ccevpc-${var.env_name}"
   cidr = var.ccevpc_cidr
 }
 
 resource "huaweicloud_vpc_subnet" "ccesubnet" {
-  name       = var.ccesubnet_name
+  name       = "ccesubnet-${var.env_name}"
   cidr       = var.ccesubnet_cidr
   gateway_ip = var.ccesubnet_gateway
 
