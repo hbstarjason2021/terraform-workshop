6929c74a4f479b9c6d6ed7bb9106844ad90b2eab
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-07-11 00:23:35 +0800

    Create variables.tf

---
+++ huaweicloud/drs-job/variables.tf
@@ -0,0 +1,39 @@
+#variable "hw_access_key" {
+#    #type = "string"
+#}
+#variable "hw_secret_key" {
+#    #type = "string"
+#}
+
+#variable "hw_region" {
+#  type    = string
+#  default = "cn-north-4"
+#}
+
+variable "vpc_name" {
+  default = "vpc-basic"
+}
+
+variable "vpc_cidr" {
+  default = "172.16.0.0/16"
+}
+
+variable "subnet_name" {
+  default = "subent-default"
+}
+
+variable "subnet_cidr" {
+  default = "172.16.10.0/24"
+}
+
+variable "subnet_gateway" {
+  default = "172.16.10.1"
+}
+
+variable "primary_dns" {
+  default = "100.125.1.250"
+}
+
+variable "secondary_dns" {
+  default = "100.125.21.250"
+}
