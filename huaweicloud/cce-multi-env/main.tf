9fc1ce78713de3e3743a6b3a42e82078ba4e05ce
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-07-10 15:31:13 +0800

    Update main.tf

--- huaweicloud/cce-multi-env/main.tf
+++ huaweicloud/cce-multi-env/main.tf
@@ -3,6 +3,12 @@
 ### terraform workspace show
 ### terraform workspace list
 ### terraform workspace select dev
+
+### terraform plan -var-file=terraform.dev.tfvars
+### terraform plan -var-file=terraform.test.tfvars
+### terraform plan -var-file=terraform.staging.tfvars
+### terraform plan -var-file=terraform.prod.tfvars
+
 ### terraform plan -var-file=terraform.dev.tfvars -out tfplan
 ### terraform apply tfplan
 
