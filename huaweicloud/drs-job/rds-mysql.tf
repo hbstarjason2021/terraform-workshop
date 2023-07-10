33fec78e15026d3e2e81d81c3982aca13b3b0a4d
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-07-11 00:15:50 +0800

    Create rds-mysql.tf

---
+++ huaweicloud/drs-job/rds-mysql.tf
@@ -0,0 +1,46 @@
+
+resource "random_password" "mypassword" {
+  length           = 12
+  special          = true
+  override_special = "!@#%^*-_=+"
+}
+
+
+resource "huaweicloud_rds_instance" "myinstance" {
+  name                = "mysql_instance"
+  flavor              = "rds.mysql.n1.large.2.ha"   ### https://support.huaweicloud.com/productdesc-rds/rds_01_0034.html
+  ha_replication_mode = "async"
+  vpc_id              = huaweicloud_vpc.myvpc.id
+  subnet_id           = huaweicloud_vpc_subnet.mysubnet.id
+  security_group_id   = huaweicloud_networking_secgroup.secgroup.id
+  availability_zone   = [
+    data.huaweicloud_availability_zones.myaz.names[0],
+    data.huaweicloud_availability_zones.myaz.names[1]
+  ]
+  db {
+    type     = "MySQL"
+    version  = "8.0"
+    #password = var.rds_password
+    password = "Huawei@123"
+    #password = random_password.mypassword.result
+  }
+  volume {
+    type = "CLOUDSSD"  ### https://registry.terraform.io/providers/huaweicloud/huaweicloud/latest/docs/resources/rds_instance
+    size = 40
+  }
+}
+
+
+resource "huaweicloud_rds_read_replica_instance" "myreplica" {
+  name                = "myreplica"
+  flavor              = "rds.mysql.n1.large.2.rr"
+  primary_instance_id = huaweicloud_rds_instance.myinstance.id
+  availability_zone   = data.huaweicloud_availability_zones.myaz.names[1]
+
+  volume {
+    type = "CLOUDSSD"
+  }
+  tags = {
+    type = "readonly"
+  }
+}
