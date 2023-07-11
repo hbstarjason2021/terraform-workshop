8de3f21dda26047e51c28a5456f14b4be0df31f2
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-07-11 22:31:58 +0800

    Create init.sh

---
+++ kind/init.sh
@@ -0,0 +1,18 @@
+
+### install terraform 
+
+TF_VERSION="1.5.2"
+curl -LO https://releases.hashicorp.com/terraform/${TF_VERSION}/terraform_${TF_VERSION}_linux_amd64.zip
+unzip terraform_${TF_VERSION}_linux_amd64.zip
+mv terraform /usr/local/bin
+terraform version   
+terraform -install-autocomplete
+
+### install kind
+
+KIND_VESION="v0.19.0"
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/${KIND_VESION}/kind-linux-amd64
+chmod +x ./kind
+mv ./kind /usr/local/bin/kind
+kind version
+
