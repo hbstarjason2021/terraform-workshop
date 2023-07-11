9033a8608cd97752ba7a0cc35feb5329d099afaa
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-07-11 10:47:07 +0800

    Update drs-mysql.tf

--- huaweicloud/drs-job/drs-mysql.tf
+++ huaweicloud/drs-job/drs-mysql.tf
@@ -1,4 +1,36 @@
 
 ### drs mysql
 
+#### https://registry.terraform.io/providers/huaweicloud/huaweicloud/latest/docs/resources/drs_job
   
+
+resource "huaweicloud_drs_job" "drs_job" {
+  direction = "up"
+  engine_type = "mysql"
+  name = "drs-test"
+  type = "migration"
+  # net_type       = "eip"
+  # migration_type = "FULL_INCR_TRANS"
+
+  destination_db {
+    engine_type = "mysql"
+    instance_id = "27d0a136f5e7452b82f11163a32c4a51in01"
+    ip = "<ip>"
+    password = "<paasword>"
+    port = 3306
+    subnet_id = "ae4c68a1-98e6-483a-bcf9-9404c55767cb"
+    user = "root"
+  }
+
+  source_db {
+    engine_type = "mysql"
+    ip = "<ip>"
+    password = "<paasword>"
+    port = 3306
+    user = "root"
+    # ssl_link    = false
+  }
+
+#####
+
+}
