0875656659df341a7c412fd0d183e53bf8ebacc5
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-07-12 11:04:45 +0800

    Update versions.tf

--- kind/versions.tf
+++ kind/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     kind = {
       source  = "tehcyx/kind"
-      version = "0.0.12"
+      version = "0.0.13"
     }
     null = {
       source  = "hashicorp/null"
