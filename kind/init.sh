b44a14edd98bee3d655191acbaf9c12b59f4b7a1
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-07-12 11:14:08 +0800

    Update init.sh

--- kind/init.sh
+++ kind/init.sh
@@ -18,6 +18,12 @@ chmod +x ./kind
 mv ./kind /usr/local/bin/kind
 kind version
 
+### install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
+/usr/local/bin/kubectl version --client
+
 ###   apt update -y && apt install docker.io -y
 
 docker pull kindest/node:v1.24.0
