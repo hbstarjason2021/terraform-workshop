d37c79a9021ada8f81a51ec9647ad091dd873900
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-07-13 10:02:06 +0800

    Create variables.tf

---
+++ huaweicloud/sms-task/variables.tf
@@ -0,0 +1,32 @@
+variable "vpc_name" {
+  #default = "vpc-basic"
+   type = string
+}
+
+variable "vpc_cidr" {
+  #default = "172.16.0.0/16"
+   type = string
+}
+
+variable "subnet_name" {
+  #default = "subent-default"
+   type = string
+}
+
+variable "subnet_cidr" {
+  #default = "172.16.10.0/24"
+   type = string
+}
+
+variable "subnet_gateway" {
+  #default = "172.16.10.1"
+   type = string
+}
+
+variable "primary_dns" {
+  default = "100.125.1.250"
+}
+
+variable "secondary_dns" {
+  default = "100.125.21.250"
+}
