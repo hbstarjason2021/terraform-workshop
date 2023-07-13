12a3ef497be21aa84ccac52750a4f7809909f51a
hbstarjason2021 <1124484297@qq.com>
2023-07-13 14:47:14 +0000

    add sms-task new env files

---
+++ huaweicloud/sms-task/new-env/provider.tf
@@ -0,0 +1,19 @@
+
+terraform {
+  required_version = ">= 1.1"
+
+  required_providers {
+    huaweicloud = {
+      #source = "huaweicloud/huaweicloud"
+      source  = "local-registry/huaweicloud/huaweicloud"
+      version = ">= 1.40.0"
+    }
+  }
+}
+
+# Configure the HuaweiCloud Provider
+provider "huaweicloud" {
+  region     = "cn-north-4"
+  #access_key = "XXX"
+  #secret_key = "XXX"
+}
