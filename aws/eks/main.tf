4fced42758f79b69e7fae19e80ab3a27907600c4
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-07-15 18:28:50 +0800

    Update main.tf

--- aws/eks/main.tf
+++ aws/eks/main.tf
@@ -26,3 +26,52 @@ resource "local_file" "kubeconfig" {
   filename = "kubeconfig_${local.cluster_name}"
 }
 
+######## https://github.com/terraform-aws-modules/terraform-aws-vpc
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  #version = "3.18.1"
+
+  name                 = "k8s-vpc"
+  cidr                 = "172.16.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
+  public_subnets       = ["172.16.4.0/24", "172.16.5.0/24", "172.16.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+}
+
+
+######## https://github.com/terraform-aws-modules/terraform-aws-eks
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name    = local.cluster_name
+  cluster_version = "1.25"
+  subnet_ids      = module.vpc.private_subnets
+
+  vpc_id = module.vpc.vpc_id
+
+  eks_managed_node_groups = {
+    first = {
+      desired_capacity = 1
+      max_capacity     = 10
+      min_capacity     = 1
+
+      instance_type = "m5.large"
+    }
+  }
+}
