0f283adb6da1ae7f02df3ddf6e5d165e82c98c1c
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-07-15 18:23:19 +0800

    Update provider.tf

--- aws/eks/provider.tf
+++ aws/eks/provider.tf
@@ -11,3 +11,9 @@ terraform {
 provider "aws" {
   region  = "ap-southeast-1"
 }
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
