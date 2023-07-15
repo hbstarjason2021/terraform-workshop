2f13cb5012f7eaabd355182d8c4696e617d2d3aa
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-07-15 18:40:30 +0800

    Update provider.tf

--- aws/eks/provider.tf
+++ aws/eks/provider.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.72.0"
+      version = ">= 3.72.0"    ##### >= 3.72.0, >= 4.24.0, >= 4.47.0, >= 4.57.0, >=5.0.0
     }
   }
 }
