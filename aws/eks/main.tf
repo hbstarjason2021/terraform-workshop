059f3a4e7d9d61822baa75e0f8b55e9b23f75df5
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-07-15 18:23:55 +0800

    Update main.tf

--- aws/eks/main.tf
+++ aws/eks/main.tf
@@ -12,3 +12,17 @@ data "aws_eks_cluster_auth" "cluster" {
 locals {
   cluster_name = "learnk8s"
 }
+
+module "eks-kubeconfig" {
+  source  = "hyperbadger/eks-kubeconfig/aws"
+  version = "1.0.0"
+
+  depends_on = [module.eks]
+  cluster_id = module.eks.cluster_id
+}
+
+resource "local_file" "kubeconfig" {
+  content  = module.eks-kubeconfig.kubeconfig
+  filename = "kubeconfig_${local.cluster_name}"
+}
+
