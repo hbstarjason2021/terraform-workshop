988a794df98aad3dbb4223b15d8ae8efa94fec0f
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-07-18 10:41:32 +0800

    Update main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -1,14 +1,4 @@
 # https://learn.hashicorp.com/terraform/kubernetes/provision-eks-cluster#optional-configure-terraform-kubernetes-provider
 # https://learn.hashicorp.com/terraform/kubernetes/deploy-nginx-kubernetes
 
-data "aws_availability_zones" "available" {}
-
-locals {
-  cluster_name = "utrains-eks-${random_string.suffix.result}"
-}
-
-resource "random_string" "suffix" {
-  length  = 6
-  special = false
-}
 
