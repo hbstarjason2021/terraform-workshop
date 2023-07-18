ce13480db7a4ab279334b8ab50700f00499c42bf
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-07-18 09:15:05 +0800

    Update variables.tf

--- aws/eks/variables.tf
+++ aws/eks/variables.tf
@@ -2,3 +2,9 @@ variable "cluster_name" {
   #type        = string
   default      = "learnk8s"
 }
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
