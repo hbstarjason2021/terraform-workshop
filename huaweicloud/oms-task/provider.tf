2a6ab961d0a70109b3d60904a8a5d63413ee4e8c
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-07-21 10:59:10 +0800

    Update provider.tf

--- huaweicloud/oms-task/provider.tf
+++ huaweicloud/oms-task/provider.tf
@@ -14,9 +14,9 @@ terraform {
 
   required_providers {
     huaweicloud = {
-      source = "huaweicloud/huaweicloud"
+      #source = "huaweicloud/huaweicloud"
       
-      ##source  = "local-registry/huaweicloud/huaweicloud"
+      source  = "local-registry/huaweicloud/huaweicloud"
       
       version = "1.51.0"
     }
