cd0efa11f4dfbe976adaff370515d11d2bcbd98d
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-07-21 10:58:56 +0800

    Create obs.tf

---
+++ huaweicloud/oms-task/obs.tf
@@ -0,0 +1,21 @@
+locals {
+	timestamp = formatdate("YYYYMMDDhhmmss", timestamp())
+	# ecs 服务器的密码
+	# ecspassword="${random_string.password.result}@wm"
+	# obs的名称
+	obsname = "obs-source-${local.timestamp}"
+  # obs桶策略
+  obspolicy = jsonencode({"Statement":[{"Sid": "公共读-${local.obsname}","Effect": "Allow","Principal": {"ID": ["*"]},"Action": ["ListBucket","HeadBucket","GetBucketLocation","ListBucketVersions","GetObject","RestoreObject","GetObjectVersion"],"Resource": ["${local.obsname}","${local.obsname}/*"]}]})
+}
+
+## https://support.huaweicloud.com/usermanual-terraform/terraform_0012.html
+# 创建obs桶
+resource "huaweicloud_obs_bucket" "obs" {
+  bucket = local.obsname
+  acl    = "public-read"
+  tags = {
+    type = "bucket"
+    env  = "${local.obsname}"
+  }
+  policy = local.obspolicy
+}
