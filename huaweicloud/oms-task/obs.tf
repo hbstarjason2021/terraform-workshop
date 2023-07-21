c7d5ab622715d98ceb9cfcaa0568c93dbe8d48ce
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-07-21 13:14:51 +0800

    Update obs.tf

--- huaweicloud/oms-task/obs.tf
+++ huaweicloud/oms-task/obs.tf
@@ -8,6 +8,28 @@ locals {
   obspolicy = jsonencode({"Statement":[{"Sid": "公共读-${local.obsname}","Effect": "Allow","Principal": {"ID": ["*"]},"Action": ["ListBucket","HeadBucket","GetBucketLocation","ListBucketVersions","GetObject","RestoreObject","GetObjectVersion"],"Resource": ["${local.obsname}","${local.obsname}/*"]}]})
 }
 
+/*
+# granting the Read-Only permission to anonymous users
+
+resource "huaweicloud_obs_bucket_policy" "policy" {
+  bucket = huaweicloud_obs_bucket.obs.bucket
+  policy = <<POLICY
+{
+  "Statement": [
+    {
+      "Sid": "AddPerm",
+      "Effect": "Allow",
+      "Principal": {"ID": "*"},
+      "Action": ["GetObject"],
+      "Resource": "mywebsite/*"
+    } 
+  ]
+}
+POLICY
+}
+
+*/
+
 ## https://support.huaweicloud.com/usermanual-terraform/terraform_0012.html
 # 创建obs桶
 resource "huaweicloud_obs_bucket" "obs" {
