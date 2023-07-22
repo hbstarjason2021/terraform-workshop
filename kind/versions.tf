2c1a1c163d82b10cfef076a4745b34fcaa645a69
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-07-22 22:41:14 +0800

    Update versions.tf

--- kind/versions.tf
+++ kind/versions.tf
@@ -6,7 +6,7 @@ terraform {
     kind = {
     
       # https://github.com/tehcyx/terraform-provider-kind/releases
-      # .terraform/providers/registry.terraform.io/tehcyx/kind/0.2.0/linux_amd64/
+      # .terraform/providers/registry.terraform.io/tehcyx/kind/0.2.0/linux_amd64/terraform-provider-kind
 
       source  = "tehcyx/kind"
       version = "0.2.0"
