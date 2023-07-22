a0cee8a83dc9e99bdc88e564856cc238b675e966
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-07-23 00:28:42 +0800

    Create argocd.tf

---
+++ kind/argocd.tf
@@ -0,0 +1,23 @@
+provider "helm" {
+  kubernetes {
+    host = "${kind_cluster.default.endpoint}"
+    cluster_ca_certificate = "${kind_cluster.default.cluster_ca_certificate}"
+    client_certificate = "${kind_cluster.default.client_certificate}"
+    client_key = "${kind_cluster.default.client_key}"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name  = "argocd"
+
+  ### helm repo add argo https://argoproj.github.io/argo-helm
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  version          = "5.41.1"
+  create_namespace = true
+
+  #values = [
+  #  file("argocd/application.yaml")
+  #]
+}
