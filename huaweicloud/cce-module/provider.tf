5ce47ad323d92ae380c55f92f523c64c91c646f8
hbstarjason2021 <1124484297@qq.com>
2023-07-24 15:55:21 +0000

    add cce-module

---
+++ huaweicloud/cce-module/provider.tf
@@ -0,0 +1,42 @@
+
+### https://support.huaweicloud.com/usermanual-terraform/terraform_0001.html
+
+## export HW_ACCESS_KEY="my-access-key"
+## export HW_SECRET_KEY="my-secret-key"
+
+
+## export hw_region="cn-north-4"
+## export hw_access_key="XXX"
+## export hw_secret_key="YYY"
+
+terraform {
+  required_version = "~> 1.4"
+
+  required_providers {
+    huaweicloud = {
+      source = "huaweicloud/huaweicloud"
+      
+      ##source  = "local-registry/huaweicloud/huaweicloud"
+      
+      version = "1.51.0"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = "3.1.1"
+    }
+  }
+}
+
+# Configure the HuaweiCloud Provider
+provider "huaweicloud" {
+  region     = "cn-north-4"
+  #access_key = "${var.hw_access_key}"
+  #secret_key = "${var.hw_secret_key}"
+}
+
+
+#provider "kubernetes" {
+#            config_path    = local_file.kube_config.filename
+#            config_context = "external"
+#}
