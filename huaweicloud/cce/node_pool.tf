7c64a3412a747959c66b98a4688e2275f949446d
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-07-24 10:43:31 +0800

    Update node_pool.tf

--- huaweicloud/cce/node_pool.tf
+++ huaweicloud/cce/node_pool.tf
@@ -2,6 +2,10 @@
 ### https://registry.terraform.io/providers/huaweicloud/huaweicloud/latest/docs/resources/cce_node_pool
 
 resource "huaweicloud_cce_node_pool" "node_pool" {
+  depends_on = [
+    huaweicloud_cce_cluster.mycce
+  ]  
+
   cluster_id               = huaweicloud_cce_cluster.mycce.id
   name                     = "myccepool"
   os                       = "EulerOS 2.9"
@@ -9,6 +13,7 @@ resource "huaweicloud_cce_node_pool" "node_pool" {
   flavor_id                = "c7.xlarge.2"
   # availability_zone        = var.availability_zone
   key_pair                 = huaweicloud_compute_keypair.mykeypair.name
+  ## password                 = var.password
   scall_enable             = true
   min_node_count           = 1
   max_node_count           = 10
