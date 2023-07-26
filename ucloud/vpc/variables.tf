221222b17acdc2059aa3b7e480316bb751fea7e4
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-07-26 15:12:56 +0800

    Update variables.tf

--- ucloud/vpc/variables.tf
+++ ucloud/vpc/variables.tf
@@ -5,3 +5,19 @@ variable "region" {
 variable "peer_region" {
   default     = "cn-sh2"
 }
+
+/*
+resource "ucloud_vpc" "foo" {
+  name        = "tf-example-vpc-01"
+  tag         = "tf-example"
+  cidr_blocks = ["192.168.0.0/16"]
+}
+
+resource "ucloud_vpc" "bar" {
+  provider    = ucloud.shanghai
+  name        = "tf-example-vpc-02"
+  tag         = "tf-example"
+  cidr_blocks = ["10.10.0.0/16"]
+}
+
+*/
