f0af35be5a7c67474792196551026694da457f3c
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-08-02 17:45:21 +0800

    Update install_agent.sh

--- huaweicloud/sms-task/install_agent.sh
+++ huaweicloud/sms-task/install_agent.sh
@@ -5,8 +5,8 @@ wget -t 3 -T 15 https://sms-agent-2-0-cn-north-1.obs.cn-north-1.myhuaweicloud.co
 # 将本内容在linux机器中生成一个.sh文件, 在SMS-Agent.tar.gz安装包目录bash执行该sh文件即可
 # ak sk sms域名替换成真正的，{}不保留
 
-AK=""
-SK=""
+HW_ACCESS_KEY=""
+HW_SECRET_KEY=""
 DOMAIN="sms.cn-north-4.myhuaweicloud.com"
 
 tar -zxvf SMS-Agent.tar.gz
@@ -28,6 +28,6 @@ fi
 if [ ! -e "linuxmain" ]; then cp -f "x64/linuxmain" . ; fi 
 
 ./linuxmain pre-check > /dev/null 2>&1
-echo $AK $SK $DOMAIN | ./linuxmain  > /dev/null 2>&1 &
+echo $HW_ACCESS_KEY $HW_SECRET_KEY $DOMAIN | ./linuxmain  > /dev/null 2>&1 &
 
 
