7d302c7176f571ee33b8973b0cec71fd1ec29816
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-08-06 01:08:45 +0800

    Update rds-mysql-destination.tf

--- huaweicloud/drs-job/rds-mysql-destination.tf
+++ huaweicloud/drs-job/rds-mysql-destination.tf
@@ -21,8 +21,8 @@ resource "huaweicloud_rds_instance" "myinstance_dest" {
     type     = "MySQL"
     version  = "5.7"
     #password = var.rds_password
-    password = "Zh9NTF8=919w"
-    #password = random_password.mypassword.result
+    #password = "Zh9NTF8=919w"
+    password = random_password.mypassword.result
   }
   volume {
     type = "CLOUDSSD"  ### https://registry.terraform.io/providers/huaweicloud/huaweicloud/latest/docs/resources/rds_instance
