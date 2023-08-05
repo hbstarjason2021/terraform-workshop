27089b76ff34614ed40c8adc4de4edce966e5cb3
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-08-06 00:33:27 +0800

    Update mysql.sh

--- huaweicloud/drs-job/mysql.sh
+++ huaweicloud/drs-job/mysql.sh
@@ -1,8 +1,17 @@
 
+### apt-get install expect
+
 mysql_host=$(ifconfig eth0 |grep "inet "| awk '{print $2}')
 
 ## mysql -u root -h $mysql_host -p -e "create database loadtest"
 ## Cloud12#$
+
+#!/usr/bin/expect
+spawn mysql -u root -h $mysql_host -p -e "create database loadtest"
+expect "*password"
+send "Cloud12#$\n"
+expect eof 
+# 上面脚本\n可用\r代替
 
 
 ### sysbench /root/sysbench/tests/include/oltp_legacy/oltp.lua --db-driver=mysql --mysql-db=loadtest --mysql-user=root --mysql-password='Cloud12#$' --mysql-port=3306 --mysql-host='$mysql_host' --oltp-tables-count=10 --oltp-table-size=10000 --threads=20 prepare
