352d2c74cd36b5040c8c2d563faec54d0bed03ab
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-08-06 00:11:16 +0800

    Create mysql.sh

---
+++ huaweicloud/drs-job/mysql.sh
@@ -0,0 +1,10 @@
+
+mysql_host=""
+
+## mysql -u root -h $mysql_host -p -e "create database loadtest"
+## Cloud12#$
+
+
+### sysbench /root/sysbench/tests/include/oltp_legacy/oltp.lua --db-driver=mysql --mysql-db=loadtest --mysql-user=root --mysql-password='Cloud12#$' --mysql-port=3306 --mysql-host='$mysql_host' --oltp-tables-count=10 --oltp-table-size=10000 --threads=20 prepare
+### sysbench /root/sysbench/tests/include/oltp_legacy/insert.lua --db-driver=mysql --mysql-db=loadtest --mysql-user=root --mysql-password='Cloud12#$' --mysql-port=3306 --mysql-host='$mysql_host' --oltp-tables-count=10 --oltp-table-size=1000 --max-time=3600 --max-requests=0 --threads=10 --report-interval=3 --rate=20 --forced-shutdown=1 run
+
