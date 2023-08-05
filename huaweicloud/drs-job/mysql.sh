fe07556e19405cf62ba040435c4dc99e4dc27c2c
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-08-06 00:50:29 +0800

    Update mysql.sh

--- huaweicloud/drs-job/mysql.sh
+++ huaweicloud/drs-job/mysql.sh
@@ -3,6 +3,7 @@
 ### apt-get install expect
 
 mysql_host=$(ifconfig eth0 |grep "inet "| awk '{print $2}')
+mysql_paas="Cloud12#$"
 
 ## mysql -u root -h $mysql_host -p -e "create database loadtest"
 ## Cloud12#$
@@ -14,6 +15,6 @@ send "Cloud12#$\r"
 interact
 
 
-### sysbench /root/sysbench/tests/include/oltp_legacy/oltp.lua --db-driver=mysql --mysql-db=loadtest --mysql-user=root --mysql-password='Cloud12#$' --mysql-port=3306 --mysql-host='$mysql_host' --oltp-tables-count=10 --oltp-table-size=10000 --threads=20 prepare
-### sysbench /root/sysbench/tests/include/oltp_legacy/insert.lua --db-driver=mysql --mysql-db=loadtest --mysql-user=root --mysql-password='Cloud12#$' --mysql-port=3306 --mysql-host='$mysql_host' --oltp-tables-count=10 --oltp-table-size=1000 --max-time=3600 --max-requests=0 --threads=10 --report-interval=3 --rate=20 --forced-shutdown=1 run
+### sysbench /root/sysbench/tests/include/oltp_legacy/oltp.lua --db-driver=mysql --mysql-db=loadtest --mysql-user=root --mysql-password="$mysql_paas" --mysql-port=3306 --mysql-host="$mysql_host" --oltp-tables-count=10 --oltp-table-size=10000 --threads=20 prepare
+### sysbench /root/sysbench/tests/include/oltp_legacy/insert.lua --db-driver=mysql --mysql-db=loadtest --mysql-user=root --mysql-password="$mysql_paas" --mysql-port=3306 --mysql-host="$mysql_host" --oltp-tables-count=10 --oltp-table-size=1000 --max-time=3600 --max-requests=0 --threads=10 --report-interval=3 --rate=20 --forced-shutdown=1 run
 
