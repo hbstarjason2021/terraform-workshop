3ec7197814e82528d2d6ecc6c5fd5c7953741298
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-08-16 14:57:19 +0800

    Create ecs-multi.tf

---
+++ huaweicloud/ecs/ecs-multi.tf
@@ -0,0 +1,69 @@
+
+resource "huaweicloud_compute_instance" "myinstance" {
+  name            = "ecs-multi-${count.index}"
+  count           = 3
+  image_id        = data.huaweicloud_images_image.myimage.id
+  flavor_id       = data.huaweicloud_compute_flavors.myflavor.ids[0]
+  #flavor_id       = "s6.xlarge.2"
+  security_groups = ["Sys-FullAccess"]
+  #security_group_ids = data.huaweicloud_networking_secgroup.mysecgroup.id
+
+  availability_zone = data.huaweicloud_availability_zones.myaz.names[3]
+  system_disk_type  = "GPSSD"
+   admin_pass         = "Huawei@1234" 
+  #key_pair              = data.huaweicloud_kps_keypairs.mykeypair.name
+  #charging_mode         = "prePaid"
+  #period_unit           = "month"
+  #period                = 1
+  #enterprise_project_id = "XXXXXXXXXXXXXXXXXXXXXXX"
+
+  network {
+    uuid = data.huaweicloud_vpc_subnet.mysubnet.id
+  }
+}
+
+##############################################################
+
+resource "huaweicloud_evs_volume" "myvol" {
+  count             = 3
+  name              = "volume_${count.index}"
+  availability_zone = data.huaweicloud_availability_zones.myaz.names[3]
+  volume_type       = "SAS"
+  size              = 500
+}
+
+resource "huaweicloud_compute_volume_attach" "attachments" {
+  count       = 4
+  instance_id = huaweicloud_compute_instance.myinstance[count.index].id
+  volume_id   = element(huaweicloud_evs_volume.myvol[*].id, count.index)
+}
+
+output "volume-devices" {
+  value = huaweicloud_compute_volume_attach.attachments[*].device
+}
+
+##################################################################
+
+/*
+resource "huaweicloud_vpc_eip" "myeip" {
+  count = 3
+  publicip {
+    type = "5_bgp"
+
+  }
+  bandwidth {
+
+    name        = "mybandwidth"
+    size        = 50
+    share_type  = "PER"
+    charge_mode = "traffic"
+  }
+}
+
+
+resource "huaweicloud_compute_eip_associate" "associated" {
+  count       = 3
+  public_ip   = huaweicloud_vpc_eip.myeip[count.index].address
+  instance_id = element(huaweicloud_vpc_eip.myeip[*].id, count.index)
+}
+*/
