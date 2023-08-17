f712b1a6cddcd6c171dbe8995f3c40b9161d5fc8
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-08-17 23:52:36 +0800

    Update main.tf

--- huaweicloud/ecs/main.tf
+++ huaweicloud/ecs/main.tf
@@ -10,24 +10,6 @@ locals {
 }
 
 
-
-resource "huaweicloud_vpc" "myvpc" {
-  name = var.vpc_name
-  cidr = var.vpc_cidr
-}
-
-resource "huaweicloud_vpc_subnet" "mysubnet" {
-  name       = var.subnet_name
-  cidr       = var.subnet_cidr
-  gateway_ip = var.subnet_gateway
-
-  # dns is required for cce node installing
-  primary_dns   = var.primary_dns
-  secondary_dns = var.secondary_dns
-  vpc_id        = huaweicloud_vpc.myvpc.id
-}
-
-
 resource "random_password" "password" {
   length           = 16
   special          = true
@@ -37,91 +19,4 @@ resource "random_password" "password" {
   min_special      = 1
 }
 
-resource "huaweicloud_compute_instance" "myinstance" {
-  name               = "ecs-basic"
-  image_id           = data.huaweicloud_images_image.myimage.id
-  flavor_id          = data.huaweicloud_compute_flavors.myflavor.ids[0]
-  security_groups   = [var.secgroup_name]
-  #security_group_ids = [huaweicloud_networking_secgroup.default.id]
-  
-  availability_zone  = data.huaweicloud_availability_zones.myaz.names[0]
-  system_disk_type   = "SSD"
-  admin_pass        = "Huawei123" 
-  #admin_pass        = random_password.password.result
-  
-  # charging_mode = "prePaid"
-  # period_unit   = "month"
-  # period        = 1
-  
-  ## wget http://mirrors.myhuaweicloud.com/repo/mirrors_source.sh && sh mirrors_source.sh
-  ## user_data = "#!/bin/bash\napt-get update -y && wget -qO- https://jihulab.com/hbstarjason/ali-init/-/raw/main/huawei_init.sh| bash"
-  user_data         = <<-EOF
-#!/bin/bash
-echo 'root:Huawei@123' | passwd root --stdin > /dev/null 2>&1
-apt-get update -y
-wget -qO- https://jihulab.com/hbstarjason/ali-init/-/raw/main/ali.sh | bash
-sleep 15
-wget -qO- https://jihulab.com/hbstarjason/ali-init/-/raw/main/huawei_init.sh| bash
-sleep 5
-EOF
 
-# 注意有坑：设置了user_data字段后，admin_pass字段将无效
-# https://github.com/huaweicloud/terraform-provider-huaweicloud/blob/master/examples/ecs/userdata/main.tf
-  
-  network {
-    uuid = huaweicloud_vpc_subnet.mysubnet.id
-  }
-}
-
-
-resource "huaweicloud_vpc_eip" "myeip" {
-  publicip {
-    type = "5_bgp"
-  }
-  bandwidth {
-    name        = "mybandwidth"
-    size        = 50
-    share_type  = "PER"
-    charge_mode = "traffic"
-  }
-}
-
-resource "huaweicloud_compute_eip_associate" "associated" {
-  public_ip   = huaweicloud_vpc_eip.myeip.address
-  instance_id = huaweicloud_compute_instance.myinstance.id
-}
-
-
-resource "huaweicloud_evs_volume" "myvolume" {
-  name              = "myvolume"
-  availability_zone = data.huaweicloud_availability_zones.myaz.names[0]
-  volume_type       = "SAS"
-  size              = 100
-}
-
-resource "huaweicloud_compute_volume_attach" "attached" {
-  instance_id = huaweicloud_compute_instance.myinstance.id
-  volume_id   = huaweicloud_evs_volume.myvolume.id
-}
-
-###############################
-/*
-resource "huaweicloud_evs_volume" "myvolume" {
-  count             = 3
-  name              = "volume_${count.index}"
-  availability_zone = data.huaweicloud_availability_zones.myaz.names[3]
-  volume_type       = "SAS"
-  size              = 100
-}
-
-
-resource "huaweicloud_compute_volume_attach" "attachments" {
-  count       = 3
-  instance_id = huaweicloud_compute_instance.myinstance[count.index].id
-  volume_id   = element(huaweicloud_evs_volume.myvolume[*].id, count.index)
-}
-
-output "volume-devices" {
-  value = huaweicloud_compute_volume_attach.attachments[*].device
-}
-*/
