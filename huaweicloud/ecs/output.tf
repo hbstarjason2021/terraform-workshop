e52ca33c27b3b30d56179cd70fe7d798a76a7521
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-08-18 00:07:37 +0800

    Update output.tf

--- huaweicloud/ecs/output.tf
+++ huaweicloud/ecs/output.tf
@@ -1,10 +1,12 @@
-/*
+
 output "slb_eip_address" {
   value = huaweicloud_vpc_eip.myeip.address
 }
-*/
 
+
+/*
 output "ecs_pass" {
   value = huaweicloud_compute_instance.myinstance.admin_pass
   sensitive = true
 }
+*/
