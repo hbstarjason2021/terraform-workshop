86b4fd17b5d3e59a0564f7f34215482086695d79
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-08-18 00:11:36 +0800

    Update output.tf

--- huaweicloud/ecs/output.tf
+++ huaweicloud/ecs/output.tf
@@ -3,6 +3,10 @@ output "slb_eip_address" {
   value = huaweicloud_vpc_eip.myeip.address
 }
 
+output "az_name_list" {
+ value = data.huaweicloud_availability_zones.myaz.names[*]
+}
+
 
 /*
 output "ecs_pass" {
