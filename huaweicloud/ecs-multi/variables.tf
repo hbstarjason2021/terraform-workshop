d9515338a1208d33b240e4b3eb579b951ddabb3d
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-08-27 03:14:30 +0800

    Update variables.tf

--- huaweicloud/ecs-multi/variables.tf
+++ huaweicloud/ecs-multi/variables.tf
@@ -9,7 +9,7 @@
 
 locals {
   timestamp    = formatdate("YYYYMMDDhhmmss", timestamp())
-  keypair_name = "keypair-zhang-${local.timestamp}"
+  #keypair_name = "keypair-zhang-${local.timestamp}"
   ecs_name     = "ecs-self-${local.timestamp}"
   vpc_name     = "vpc-self-${local.timestamp}"
   subnet_name  = "subnet_name-${local.timestamp}"
