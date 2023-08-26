9af694244f29fab0c110891f1e73001bf731a3b7
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-08-27 00:32:20 +0800

    Create smn.tf

---
+++ huaweicloud/serverless/smn.tf
@@ -0,0 +1,33 @@
+resource "huaweicloud_smn_topic" "smn" {
+  name = "smn-${var.name}"
+  display_name = "demo smn-${var.name}"
+}
+
+data "huaweicloud_dcs_instances" "dcs_instance" {
+  name   = "dcs-redis-fq-demo"
+  status = "RUNNING"
+}
+
+resource "huaweicloud_ces_alarmrule" "alarm_rule" {
+  alarm_name = "alarm-rule-${var.name}"
+  metric {
+    namespace   = "SYS.DCS"
+    metric_name = "used_memory"
+    dimensions {
+      name  = "dcs_instance_id"
+      value = data.huaweicloud_dcs_instances.dcs_instance.instances[0].id
+    }
+  }
+  condition {
+    period              = 300
+    filter              = "average"
+    comparison_operator = ">"
+    value               = 8000000
+    unit                = "byte"
+    count               = 1
+  }
+  alarm_actions {
+    type              = "notification"
+    notification_list = [huaweicloud_smn_topic.smn.id]
+  }
+}
