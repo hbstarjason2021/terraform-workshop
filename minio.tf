822503f3247bef42b1e042d39265dbe1fbea263b
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-08-29 14:23:42 +0800

    Update minio.tf

--- minio.tf
+++ minio.tf
@@ -1,6 +1,10 @@
 
 terraform {
   required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.1"
+    }
     minio = {
       source  = "aminueza/minio"
       version = "1.7.1"
@@ -8,12 +12,59 @@ terraform {
   }
 }
 
+##################################################################
+
+provider "docker" {
+}
+
+resource "docker_network" "network" {
+  name = "dev"
+}
+
+resource "docker_volume" "minio_volume" {
+  name = "minio"
+}
+
+resource "docker_image" "minio_image" {
+  name = "minio/minio:latest"
+  keep_locally = true
+}
+
+resource "docker_container" "minio_container" {
+  image = docker_image.minio_image.image_id
+  name  = "minio"
+  hostname = "minio"
+  restart = "always"
+  networks_advanced {
+    name = docker_network.network.name
+  }
+  ports {
+    internal = 9010
+    external = 9010
+  }
+  ports {
+    internal = 9011
+    external = 9011
+  }
+  mounts {
+    type = "volume"
+    target = "/data"
+    source = "minio"
+  }
+  env = [
+   "MINIO_ROOT_USER=minioadmin",
+   "MINIO_ROOT_PASSWORD=minioadmin"
+  ]
+  command = ["server", "/data", "--console-address", ":9011", "--address", ":9010"]
+  remove_volumes = false
+}
+
+##################################################################
 /*
 export MINIO_ENDPOINT="172.30.1.2:9010"
 export MINIO_USER="minioadmin"
 export MINIO_PASSWORD="minioadmin"
 */
-
 
 // @see https://registry.terraform.io/providers/aminueza/minio/latest/docs
 provider "minio" {
@@ -26,7 +77,6 @@ resource "minio_s3_bucket" "state_terraform_s3" {
   bucket = "test-bucket-new"
   acl    = "public"
 }
-
 
 resource "minio_s3_object" "txt_file" {
   depends_on = [minio_s3_bucket.state_terraform_s3]
