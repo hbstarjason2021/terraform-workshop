667876155526aed849fb629ee55bf654e2347ebf
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-09-05 17:37:54 +0800

    Update mysql.sh

--- huaweicloud/drs-job/new-env/mysql.sh
+++ huaweicloud/drs-job/new-env/mysql.sh
@@ -15,6 +15,7 @@ send "Cloud12#$\r"
 interact
 
 #####  echo "create database loadtest" | mysql -u root -h  $mysql_host -p${mysql_paas}
+#####  echo "show databases" | mysql -u root -h  $mysql_host -p${mysql_paas}
 
 ### sysbench /root/sysbench/tests/include/oltp_legacy/oltp.lua --db-driver=mysql --mysql-db=loadtest --mysql-user=root --mysql-password="$mysql_paas" --mysql-port=3306 --mysql-host="$mysql_host" --oltp-tables-count=10 --oltp-table-size=10000 --threads=20 prepare
 ### sysbench /root/sysbench/tests/include/oltp_legacy/insert.lua --db-driver=mysql --mysql-db=loadtest --mysql-user=root --mysql-password="$mysql_paas" --mysql-port=3306 --mysql-host="$mysql_host" --oltp-tables-count=10 --oltp-table-size=1000 --max-time=3600 --max-requests=0 --threads=10 --report-interval=3 --rate=20 --forced-shutdown=1 run
