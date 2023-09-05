4028b816d3f9fec6e30a49496676862e6783cba8
hbstarjason2021 <1124484297@qq.com>
2023-09-05 16:11:34 +0000

    add multi-job files

---
+++ huaweicloud/drs-job/multi-job/secgroup.tf
@@ -0,0 +1,58 @@
+# 创建安全组
+resource "huaweicloud_networking_secgroup" "secgroup" {
+  #name = "my_secgroup_${local.timestamp}"
+  name = "my_secgroup_drs"
+}
+# 安全组规则-开放80端口
+resource "huaweicloud_networking_secgroup_rule" "secgroup_rule_80" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  ports             = 80
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = huaweicloud_networking_secgroup.secgroup.id
+}
+# 安全组规则-开放8081端口，用于websocket通道
+resource "huaweicloud_networking_secgroup_rule" "secgroup_rule_8081" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  ports             = 8081
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = huaweicloud_networking_secgroup.secgroup.id
+}
+# 安全组规则-开放22端口
+resource "huaweicloud_networking_secgroup_rule" "secgroup_rule_22" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  ports             = 22
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = huaweicloud_networking_secgroup.secgroup.id
+}
+# 安全组规则-开放443端口
+resource "huaweicloud_networking_secgroup_rule" "secgroup_rule_443" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  ports             = 443
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = huaweicloud_networking_secgroup.secgroup.id
+}
+# 安全组规则-开放3306端口
+resource "huaweicloud_networking_secgroup_rule" "secgroup_rule_3306" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  ports             = 3306
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = huaweicloud_networking_secgroup.secgroup.id
+}
+# 安全组规则-允许ping程序测试弹性云服务器的连通性
+resource "huaweicloud_networking_secgroup_rule" "secgroup_rule_ping" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "icmp"
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = huaweicloud_networking_secgroup.secgroup.id
+}
---
+++ huaweicloud/drs-job/multi-job/variables.tf
@@ -0,0 +1,45 @@
+#variable "hw_access_key" {
+#    #type = "string"
+#}
+#variable "hw_secret_key" {
+#    #type = "string"
+#}
+
+#variable "hw_region" {
+#  type    = string
+#  default = "cn-north-4"
+#}
+
+
+variable "rds_password" {
+  default = "Zh9NTF8919w"
+}
+
+
+variable "vpc_name" {
+  default = "vpc-source"
+}
+
+variable "vpc_cidr" {
+  default = "172.16.0.0/16"
+}
+
+variable "subnet_name" {
+  default = "subnet-source"
+}
+
+variable "subnet_cidr" {
+  default = "172.16.10.0/24"
+}
+
+variable "subnet_gateway" {
+  default = "172.16.10.1"
+}
+
+variable "primary_dns" {
+  default = "100.125.1.250"
+}
+
+variable "secondary_dns" {
+  default = "100.125.21.250"
+}
